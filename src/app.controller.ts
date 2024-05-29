65834d2cc21a661137f874b49033f85cb4828bb8
taekyun-yoon <tk.yoon0105@gmail.com>
2024-05-29 17:09:19 +0900

    feat: swagger, httpexceptionfilter 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, HttpException, HttpStatus, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Ip } from './decorators/ip.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Ip() ip: string): string {
+    console.log(`${ip}`);
+    // return this.appService.getHello();
+    throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+  }
+
+  @Get('name')
+  getName(
+    @Query('name') name: string
+  ): string {
+    return `${name} hello `;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './exceptions/http.exceptions';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  app.useGlobalFilters(new HttpExceptionFilter);
+  const config = new DocumentBuilder()
+    .setTitle('Nest-Board')
+    .setDescription('The Nest-Board API description')
+    .setVersion('1.0')
+    .addTag('Board')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
